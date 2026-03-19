c51ab88ac69f931c8f3158ca8da8c80dd9e8f343
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-03-20 00:41:29 +0100

    Add Astro configuration, TypeScript support, and update dependencies in package.json

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+  adapter: vercel(),
+})
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   vue: false,
-
+  astro: true,
   rules: {
     'style/brace-style': ['error', '1tbs'],
   },
